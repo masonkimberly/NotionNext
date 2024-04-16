29b1e56292c7004a0ba3d13a03f52a6e4e626527
masonkimberly <166720594+masonkimberly@users.noreply.github.com>
2024-04-16 15:35:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,11 +26,11 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '润润泉', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '润润泉', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '润润泉, 产品', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '1300363316@qq.com', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_WECHAT: process.env.NEXT_PUBLIC_CONTACT_WECHAT || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
