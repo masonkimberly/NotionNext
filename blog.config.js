73dc5218dc51e0aba1a17452fd8fb8d858f93691
masonkimberly <166720594+masonkimberly@users.noreply.github.com>
2024-11-30 12:33:02 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
     '专门为荒漠地区栽树研发的，土壤下直接供水植物根部的专利产品,en:Specifically developed for tree planting in desert areas and Supply water directly to the roots of plants under the soil.'
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+    CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '润润泉', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '润润泉', // 作者简介
