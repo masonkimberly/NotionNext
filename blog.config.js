217a481ca6008cc527382e9f7175caccbb2f9cb0
masonkimberly <166720594+masonkimberly@users.noreply.github.com>
2024-11-30 15:07:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -23,10 +23,11 @@ process.env.NEXT_PUBLIC_GREETING_WORDS ||
 
 CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '润润泉', // 您的昵称 例如 tangly1024
-BIO: process.env.NEXT_PUBLIC_BIO || '润润泉', // 作者简介
-LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '润润泉, 产品', // 网站关键词 英文逗号隔开
+AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'runrunquan', // 您的昵称 例如 tangly1024
+BIO: process.env.NEXT_PUBLIC_BIO || 'runrunquan', // 作者简介
+LINK: process.env.NEXT_PUBLIC_LINK || 'https://runrunquan.com', // 网站地址
+KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'runruunquan,irrigator,Soil Moisture Conservation 
+Irrigation Device,save water,润润泉,产品', // 网站关键词 英文逗号隔开
 
 // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
 CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '1300363316@qq.com', // 邮箱地址 例如mail@tangly1024.com
